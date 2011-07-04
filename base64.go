31ed52610675012a18038423161dcce43ad9e450
James Keane <james.keane@gmail.com>
2011-07-04 03:53:23 -0400

    A good days work :)

---
+++ base64.go
@@ -0,0 +1,163 @@
+package bcrypt;
+
+import (
+	"fmt"
+	"bytes"
+	"os"
+)
+
+// Table for Base64 encoding
+var base64_code = [...]byte {
+	'.', '/', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+	'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+	'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+	'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+	'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5',
+	'6', '7', '8', '9'};
+
+// TODO: This needs to be a map, i.e. index_64 := map[byte] byte{ ... }
+// Table for Base64 decoding
+var index_64 = []int8 {
+		-1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+		-1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+		-1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+		-1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+		-1, -1, -1, -1, -1, -1, 0, 1, 54, 55,
+		56, 57, 58, 59, 60, 61, 62, 63, -1, -1,
+		-1, -1, -1, -1, -1, 2, 3, 4, 5, 6,
+		7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+		17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
+		-1, -1, -1, -1, -1, -1, 28, 29, 30,
+		31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+		41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+		51, 52, 53, -1, -1, -1, -1, -1};
+
+
+
+
+// Base64
+
+func encode_base64(d []byte, leng int) (string) {
+	off := 0
+	rs := bytes.NewBufferString("")
+
+	if leng <= 0 || leng > len(d) {
+		// TODO: not really necessary, as it is a private function. But....
+		return ""//, os.NewError("Invalid len")
+	}
+	
+	for off < leng {
+		c1 := d[off] & 0xff; off++;
+		fmt.Fprint(rs, string(base64_code[(c1 >> 2) & 0x3f]));
+		c1 = (c1 & 0x03) << 4;
+		if (off >= leng) {
+			fmt.Fprint(rs, string(base64_code[c1 & 0x3f]));
+			break;
+		}
+		c2 := d[off] & 0xff; off++;
+		c1 |= (c2 >> 4) & 0x0f;
+		fmt.Fprint(rs, string(base64_code[c1 & 0x3f]));
+		c1 = (c2 & 0x0f) << 2;
+		if (off >= leng) {
+			fmt.Fprint(rs, string(base64_code[c1 & 0x3f]));
+			break;
+		}
+		c2 = d[off] & 0xff; off++
+		c1 |= (c2 >> 6) & 0x03;
+		fmt.Fprint(rs, string(base64_code[c1 & 0x3f]));
+		fmt.Fprint(rs, string(base64_code[c2 & 0x3f]));
+	}
+	return string(rs.Bytes())
+}
+/*
+func encode_base64(d []byte) (string) {
+	off := 0;
+	rs := bytes.NewBufferString("");
+	len := len(d);
+	
+	for off < len {
+		c1 := d[off] & 0xff; off++;
+		fmt.Fprint(rs, string(base64_code[(c1 >> 2) & 0x3f]));
+		c1 = (c1 & 0x03) << 4;
+		if (off >= len) {
+			fmt.Fprint(rs, string(base64_code[c1 & 0x3f]));
+			break;
+		}
+		c2 := d[off] & 0xff; off++;
+		c1 |= (c2 >> 4) & 0x0f;
+		fmt.Fprint(rs, string(base64_code[c1 & 0x3f]));
+		c1 = (c2 & 0x0f) << 2;
+		if (off >= len) {
+			fmt.Fprint(rs, string(base64_code[c1 & 0x3f]));
+			break;
+		}
+		c2 = d[off] & 0xff; off++
+		c1 |= (c2 >> 6) & 0x03;
+		fmt.Fprint(rs, string(base64_code[c1 & 0x3f]));
+		fmt.Fprint(rs, string(base64_code[c2 & 0x3f]));
+	}
+	return string(rs.Bytes())
+}*/
+	
+func char64(x byte) (byte, os.Error) {
+	if (int(x) < len(index_64)) {
+		rs := index_64[x];
+		if( rs >= 0 ) {
+			return byte(rs), nil
+		}
+	}
+	
+	return 0, os.NewError("bcrypt: Invalid base64 character")
+}
+
+func decode_base64(s string, maxolen uint) ([]byte, os.Error) {
+	ret := bytes.NewBufferString("");
+	olen := uint(0);
+	off := 0;
+	slen := len(s);
+	
+	for (off < (slen - 1) && olen < maxolen) {
+		c1, err := char64(s[off]); off++;
+		if( err != nil ) {
+			return nil, err;
+		}
+		
+		c2, err := char64(s[off]); off++;
+		if( err != nil ) {
+			return nil, err
+		}
+		
+		o := byte(c1 << 2);
+		o |= (c2 & 0x30) >> 4;
+		ret.WriteByte(o);
+		olen++;
+		if (olen >= maxolen || off >= slen) {
+			break;
+		}
+		
+		c3, err := char64(s[off]); off++;
+		if( err != nil ) {
+			return nil, err
+		}
+		
+		o = byte((c2 & 0x0f) << 4);
+		o |= (c3 & 0x3c) >> 2;
+		ret.WriteByte(o);
+		olen++;
+		if (olen >= maxolen || off >= slen) {
+			break;
+		}
+			
+		c4, err := char64(s[off]); off++;
+		if( err != nil ) {
+			return nil, err
+		}
+		o = byte((c3 & 0x03) << 6);
+		o |= c4;
+		ret.WriteByte(o);
+		olen++;
+	}
+
+	return ret.Bytes(), nil
+}
+
---
+++ bcrypt.go
@@ -0,0 +1,182 @@
+package bcrypt
+
+import (
+	"os"
+	"fmt"
+	"bytes"
+	"strconv"
+	"strings"
+	"bufio"
+	"crypto/rand"
+)
+
+var (
+        InvalidRounds = os.NewError("invalid rounds")
+        InvalidSalt   = os.NewError("invalid salt")
+)
+
+const (        
+	MaxRounds     = 31
+	MinRounds     = 4
+	DefaultRounds = 12
+    SaltLen = 16
+    BlowfishRounds = 16
+)
+
+// Helper function to build the bcrypt hash string
+func build_bcrypt_str(minor byte, rounds uint, payload ...string) string {
+	rs := bytes.NewBufferString("");
+	fmt.Fprint(rs, "$2");
+	if( minor >= 'a' ) {
+		fmt.Fprint(rs, string(minor));
+	}
+	fmt.Fprint(rs, "$");
+	if( rounds < 10 ) {
+		fmt.Fprint(rs, "0");
+	}
+	fmt.Fprint(rs, rounds);
+	fmt.Fprint(rs, "$");
+	for _, p := range payload {
+		fmt.Fprint(rs, string(p));
+	}
+	return string(rs.Bytes());
+}
+
+// Salt generation
+func Salt(rounds ...int) (string, os.Error) {        
+	r := DefaultRounds
+    if len(rounds) > 0 {
+    	r = rounds[0]
+    	if r < MinRounds || r > MaxRounds {
+			return "", InvalidRounds
+		} 
+    }
+    
+	rnd := make([]byte, SaltLen)
+	read, err := rand.Read(rnd)
+	if(read != SaltLen || err != nil) {
+		return "", os.NewError("bcrypt: Could not read the required random bytes")
+	}
+
+	return build_bcrypt_str('a', uint(r), encode_base64(rnd, len(rnd))), nil
+}
+
+// SaltBytes provides a []byte based wrapper to Salt.
+//
+func SaltBytes(rounds int) (salt []byte, err os.Error) {
+        b, err := Salt(rounds)
+        return []byte(b), err
+}
+
+func consume(r *bufio.Reader, b byte) bool {
+	got, err := r.ReadByte()
+	if err != nil {
+		return false
+	}
+	if got != b {
+		r.UnreadByte()
+		return false
+	}
+	
+	return true
+} 
+
+func Hash(password string, salt ...string) (hash string, err os.Error) {
+	var s string
+	
+	if len(salt) == 0 {
+		s, err = Salt()
+		if err != nil {
+			return
+		}
+	} else if len(salt) == 1 {
+		s = salt[0]
+	} else {
+		return "", InvalidSalt
+	}
+	
+	// Ok, extract the required information
+	minor := byte(0);
+	sr := bufio.NewReader(strings.NewReader(s))
+	
+	if !consume(sr, '$') || !consume(sr, '2') {
+		return "", InvalidSalt
+	}
+	
+	if !consume(sr, '$') {
+		minor, _ = sr.ReadByte()
+		if minor != 'a' || !consume(sr, '$') {
+			return "", InvalidSalt
+		}
+	}
+	
+	rounds_bytes := make([]byte, 2)
+	read, err := sr.Read(rounds_bytes)
+	if err != nil || read != 2 {
+		return "", InvalidSalt
+	}
+	
+	if !consume(sr, '$') {
+		return "", InvalidSalt
+	}
+	
+	var rounds uint
+	rounds, err = strconv.Atoui(string(rounds_bytes))
+	if err != nil {
+		return "", InvalidSalt
+	}
+	
+	salt_bytes := make([]byte, 22)
+	read, err = sr.Read(salt_bytes)
+	if err != nil || read != 22 {
+		return "", InvalidSalt
+	}
+	
+	var saltb []byte
+	real_salt := string(salt_bytes)
+	saltb, err = decode_base64(real_salt, SaltLen);
+	if err != nil {
+		return "", InvalidSalt
+	}
+
+	// TODO: ARGH	
+	password += "\000"
+	
+	B := newCipher();
+	hashed := B.crypt_raw([]byte(password), saltb, rounds);
+	return build_bcrypt_str(minor, rounds, real_salt, encode_base64(hashed, len(bf_crypt_ciphertext) * 4 - 1)), nil;	
+}
+
+// HashBytes provides a []byte based wrapper to Hash.
+//
+func HashBytes(password []byte, salt ...[]byte) (hash []byte, err os.Error) {
+        var s string
+        if len(salt) == 0 {
+                s, err = Hash(string(password))
+        } else {
+                s, err = Hash(string(password), string(salt[0]))
+        }
+        return []byte(s), err
+}
+
+
+// Match determines if an unencrypted password matches a previously encrypted
+// password. It does so by generating a Blowfish encrypted hash of the
+// unencrypted password and the random salt from the previously encrypted
+// password.
+//
+// Returns 'true' when the encrypted passwords match, otherwise 'false'.
+//
+func Match(password, hash string) bool {
+        h, err := Hash(password, hash)
+        if err != nil {
+                return false
+        }
+        return h == hash
+}
+
+// MatchBytes provides a []byte based wrapper to Match.
+//
+func MatchBytes(password, hash []byte) bool {
+        return Match(string(password), string(hash))
+}
